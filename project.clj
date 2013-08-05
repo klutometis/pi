26ba4c5c8c9aeed20e64948fd00250278b87bf67
Peter Danenberg <pcd@roxygen.org>
2013-08-05 02:37:41 -0700

    0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject pi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject pi "0.1.0"
+  :description "Trivial example for the first TrueCar Clojure lunch"
+  :url "https://github.com/klutometis/pi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
